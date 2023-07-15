6a8fa58d97d816ff7ac1241294d8bd33141eac2b
UngraduateGamer <139570725+UngraduateGamer@users.noreply.github.com>
2023-07-15 14:58:35 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,23 @@
+a=16
+b=3
+
+# (+) add operator
+print("Addition of the two numbers is",a+b)
+
+# (-) Substract operator
+print("Substract of the two numbers is",a-b)
+
+# (*) Multiplication operator
+print("Multiplication of the two numbers is",a*b)
+
+# (/) Division operator
+print("Division of the two numbers is",a/b)
+
+# (**) Exponent operator    eg=2**3 = 8 (2x2x2)
+print("Exponent =",a**b)
+
+# (%) Modulus operator
+print("Modules of the two numbers is",a%b)
+
+# (//) Float Division operator  eg=5//3 = 1(float values not printed)
+print("Float division of the two numbers is",a//b)
